efc4a0d38478e99444f773ef580befacf17e7c0f
Marek Biela <marekbiela26@gmail.com>
2026-03-08 16:12:40 +0100

    Solution

--- app/cafe.py
+++ app/cafe.py
@@ -19,8 +19,6 @@ class Cafe:
                 > visitor.get("vaccine").get("expiration_date")
         ):
             raise OutdatedVaccineError("Vaccine is expired")
-        missing_masks = 0
         if visitor.get("wearing_a_mask") is False:
-            missing_masks += 1
             raise NotWearingMaskError("Masks are missing")
         return f"Welcome to {self.name}"
